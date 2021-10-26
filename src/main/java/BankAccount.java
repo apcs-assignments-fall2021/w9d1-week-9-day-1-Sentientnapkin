e0fe2a4e2fc5b17dab45f87bd775f21071642fdf
Sentientnapkin <24svargas@athenian.org>
2021-10-26 10:37:54 -0700

    all methods working

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,73 @@
 public class BankAccount {
     // Instance variables
-    private int balance;
-    private int accountNumber;
+    public double balance;
+    private final int accountNumber;
     private String preferredName;
+    private double lastValue;
+    private double[] withdrawals;
+    private int count = 0;
+
+    public BankAccount(String preferredName){
+        this.balance = 2.50;
+        this.accountNumber=generateAccountNumber();
+        this.lastValue=this.balance;
+        this.withdrawals=new double[6];
+        this.preferredName = preferredName;
+    }
+    public int generateAccountNumber(){
+        int num;
+        int accountNumber = 0;
+        for(int i = 1;i<=80;i+=10){
+            num=(int)(Math.random()*9+1);
+            num*=i;
+            accountNumber+=num;
+        }
+        return accountNumber;
+    }
+    public void withdraw(double num){
+        if(num<=balance){
+            System.out.println("You have withdrawn $"+num+".");
+            balance-=num;
+        }
+        else System.out.println("You have inputted an invalid number.");
+    }
+    public void deposit(double num){
+        if(num<=balance){
+            System.out.println("You deposited $"+num+".");
+            balance+=num;
+        }
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    public String getPreferredName(){
+        return this.preferredName;
+    }
+    public void setPreferredName(String change){
+       this.preferredName=change;
+    }
+    @Override
+    public String toString(){
+        return "Owner:"+this.preferredName+"; Balance:"+this.balance+"; Account Number:"+this.accountNumber;
+    }
+    public void withdrawalHistory(){
+        this.withdrawals[count]=this.balance-this.lastValue;
+        count++;
+        double temp = 0;
+        if (count==6){
+            count--;
+            for(int i = 0;i<5;i++){
+                this.withdrawals[count]=this.withdrawals[count+1];
+            }
+        }
+        for(int i = 0;i<4;i++){
+            if(this.withdrawals[i]!=0)
+                System.out.println(this.withdrawals[i]+"; ");
+        }
+        if(this.withdrawals[4]!=0)
+            System.out.println(withdrawals[4]);
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class BankClient {
@@ -6,7 +7,48 @@ public class BankClient {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
-//        // Create a bank account with the given name and the account number 101134
-//        BankAccount account = new BankAccount(name, 101134);
+        System.out.println(name);
+        BankAccount newBank = new BankAccount(name);
+        LocalTime time = LocalTime.now();
+        System.out.println("Hello "+newBank.getPreferredName()+". The time is "+time+". Yes those are a lot of milliseconds.");
+        while(true) {
+            System.out.println("Use keywords: withdraw/deposit/check balance/check account number/set preferred name/get preferred name/withdrawal history/end session to access your account.");
+            String input = scan.nextLine();
+            if (input.equals("withdraw")) {
+                System.out.println("How much?");
+                String money = scan.nextLine();
+                int withdrawals = Integer.parseInt(money);
+                newBank.withdraw(withdrawals);
+            }
+            else if (input.equals("deposit")) {
+                System.out.println("How much?");
+                String money = scan.nextLine();
+                int deposits = Integer.parseInt(money);
+                newBank.deposit(deposits);
+                System.out.println("You deposited $"+deposits+".");
+            }
+            else if (input.equals("check balance")) {
+                System.out.println(newBank.getBalance());
+            }
+            else if (input.equals("check account number")) {
+                System.out.println(newBank.getAccountNumber());
+            }
+            else if (input.equals("check preferred name")) {
+                System.out.println(newBank.getPreferredName());
+            }
+            else if (input.equals("set preferred name")) {
+                System.out.println("What would like to change your preferred name to?");
+                String newName = scan.nextLine();
+                newBank.setPreferredName(newName);
+            }
+            else if (input.equals("withdrawal history")) {
+                newBank.withdrawalHistory();
+            }
+            else if (input.equals("end session")) {
+                break;
+            }
+            else
+                System.out.println("You have entered something invalid.");
+        }
     }
 }
